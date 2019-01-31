2e30f4ec1fc7ae60d6b0e7b1f421ecc6d9bb0f4f
Web_Developer186 <TheTroller186@gmail.com>
2019-01-31 13:17:01 +0500

    upd

--- web/index.php
+++ web/index.php
@@ -10,16 +10,33 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+
+
+$app->post('/bot', function() use($app) {
+  $data = json_decode(file_get_contents('php://input'));
+  
+  if(!data) 
+    return "bad";
+
+  if($data->secret !== getenv('VK_SECRET') && $data->type !== 'comfirmation' )
+    return "bad";
+
+  switch($data->type)
+  {
+    case 'comfirmation': 
+      return getenv('VK_SECRET');
+      break;
+    case 'message_new': 
+
+      break;
+  }
+
+
+  return "bad";
 });
 
 $app->run();
