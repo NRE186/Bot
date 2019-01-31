bb9ef4046d4451dafbbad331942d25bbbaa62088
Web_Developer186 <TheTroller186@gmail.com>
2019-01-31 14:33:31 +0500

    upd6

--- web/index.php
+++ web/index.php
@@ -33,7 +33,7 @@ $app->post('/bot', function() use($app) {
     case 'message_new':
       $request_params = array(
         'random_id' => '123',
-        'user_id'    => $data->object->from_id,
+        'peer_id'    => $data->object->from_id,
         'message'    => 'Здорова',
         'access_token' => getenv('VK_TOKEN'),
         'v' => '5.92'
