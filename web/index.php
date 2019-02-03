1ac2d93d018713a3ee84c29e1f97415b751d9d23
Web_Developer186 <TheTroller186@gmail.com>
2019-02-03 20:32:48 +0500

    try to time

--- web/index.php
+++ web/index.php
@@ -19,22 +19,6 @@ $app->get('/', function() use($app) {
 $app->post('/bot', function() use($app) {
 
   $data = json_decode(file_get_contents('php://input'));
-  $x = 0;
-  if(strtotime(date('H:i')) == strtotime('20:26') && $x == 0){
-    $request_params = array(
-      'random_id' => rand(0, 100000000000000000),
-      'peer_id'    => '104268893',
-      'message'    => 'Тест',
-      'access_token' => getenv('VK_TOKEN'),
-      'v' => '5.92'
-    );
-  
-    file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
-    $x = 1;
-    return "ok";
-    http_response_code(200);
-  }
-
   switch( $data->type )
   {
     case 'confirmation':
@@ -174,6 +158,25 @@ $app->post('/bot', function() use($app) {
       file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
       return "ok";
       http_response_code(200);
+      break;
+
+      case 'message_reply' : 
+
+      $x = 0;
+      if(strtotime(date('H:i')) == strtotime('20:34') && $x == 0){
+        $request_params = array(
+          'random_id' => rand(0, 100000000000000000),
+          'peer_id'    => '104268893',
+          'message'    => 'Тест',
+          'access_token' => getenv('VK_TOKEN'),
+          'v' => '5.92'
+        );
+      
+        file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
+        $x = 1;
+        return "ok";
+        http_response_code(200);
+      }
       break;
   }
   return "ok";
